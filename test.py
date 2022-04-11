b82c30a1e17c35e4c3a75abec386e89bde686ce5
mahahahajan <pulkit8.mahajan@gmail.com>
2022-04-11 15:40:13 -0500

    some small UI changes pt3

--- test.py
+++ test.py
@@ -6,19 +6,19 @@ os.environ['NO_PROXY'] = '127.0.0.1'
 url = 'https://power-detector.herokuapp.com/getData'
 headers = { "content-type": "application/json", "charset": 'utf-8'}
 
-val1 = {"value" : '0'}
+val1 = {"value" : '3'}
 val2 = {"value" : '4'}
-val3 = {"value" : '6'}
-myobj = {"value" : '22'}
+val3 = {"value" : '5'}
+myobj = {"value" : '32'}
 
 
 while 1:
     x = requests.post(url, headers=headers, json=val1)
     print(x)
     time.sleep(5)
-    # x = requests.post(url, headers=headers, json=val3)
-    # print(x)
-    # time.sleep(5)
-    # x = requests.post(url, headers=headers, json=myobj)
-    # print(x)
-    # time.sleep(5)
+    x = requests.post(url, headers=headers, json=val3)
+    print(x)
+    time.sleep(5)
+    x = requests.post(url, headers=headers, json=myobj)
+    print(x)
+    time.sleep(5)
