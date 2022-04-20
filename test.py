07bb9ada80b1fe611d21b4d2629d2f8d40acf6cf
mahahahajan <pulkit8.mahajan@gmail.com>
2022-04-20 00:29:40 -0700

    need to put everything into 1 repo

--- test.py
+++ test.py
@@ -9,16 +9,16 @@ headers = { "content-type": "application/json", "charset": 'utf-8'}
 val1 = {"value" : '3'}
 val2 = {"value" : '4'}
 val3 = {"value" : '5'}
-myobj = {"value" : '32'}
+myobj = {"value" : '1'}
 
 
 while 1:
-    x = requests.post(url, headers=headers, json=val1)
-    print(x)
-    time.sleep(5)
-    x = requests.post(url, headers=headers, json=val3)
-    print(x)
-    time.sleep(5)
+    # x = requests.post(url, headers=headers, json=val1)
+    # print(x)
+    # time.sleep(5)
+    # x = requests.post(url, headers=headers, json=val3)
+    # print(x)
+    # time.sleep(5)
     x = requests.post(url, headers=headers, json=myobj)
     print(x)
     time.sleep(5)
